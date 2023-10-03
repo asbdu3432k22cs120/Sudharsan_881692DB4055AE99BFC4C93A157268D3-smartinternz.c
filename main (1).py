ab2f6cf5715d424aaede71f1198a6bca9fa549c3
asbdu3432k22cs120 <145149711+asbdu3432k22cs120@users.noreply.github.com>
2023-10-03 07:08:54 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,21 @@
+def linearSearchProduct(ProductList, targetproduct):
+  indices = []
+
+  for index, product in enumerate(productList):
+    if product == targetproduct:
+      indices.append(index)
+
+  return indices
+
+
+protucts = [
+    "shoes",
+    "boot",
+    "loafer",
+    "shoes",
+    "sandal",
+    "shoes",
+]
+target = "shoes"
+result = linearSearchProduct(products, target)
+print(result)
